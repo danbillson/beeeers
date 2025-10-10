50bf2add95a736536dd7f2d00946fce104bf2446
Dan Billson <dbillson@outlook.com>
2025-10-10 22:49:25 +0100

    feat: use scaled theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased relative h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative h-screen theme-scaled`}
       >
         <Dots />
         {children}
